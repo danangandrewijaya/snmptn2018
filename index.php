3c476822fc3f61ec93fe49f33f0c2bafd2d2e210
Danang Andre Wijaya <danangandrewijaya@gmail.com>
2018-03-12 00:26:26 +0700

    fix

--- index.php
+++ index.php
@@ -365,7 +365,7 @@
      
      $(document).on('click', '[data-toggle="lightbox"]', function(event) {
                 event.preventDefault();
-                $(this).ekkoLightbox();
+                $(this).ekkoLightbox({wrapping: false});
             });
   
  });
